5b3317609dfb48fc9bb3ebb46f758101b822c5b4
masterUNG <phrombutr@gmail.com>
2016-03-30 09:11:35 +0700

    Finish 30March16

---
+++ app/src/main/java/appewtc/masterung/resortball/MyAlertDialog.java
@@ -0,0 +1,16 @@
+package appewtc.masterung.resortball;
+
+import android.content.Context;
+
+/**
+ * Created by masterUNG on 3/30/16 AD.
+ */
+public class MyAlertDialog {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+
+    }
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/resortball/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/resortball/SignUpActivity.java
@@ -2,12 +2,61 @@ package appewtc.masterung.resortball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText,
+            nameEditText, idCardEditText;
+    private String userString, passwordString,
+            nameString, idCardString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+
+    }       //Main Method
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+        nameEditText = (EditText) findViewById(R.id.editText3);
+        idCardEditText = (EditText) findViewById(R.id.editText4);
     }
-}
+
+    public void clickSignUpSign(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+        nameString = nameEditText.getText().toString().trim();
+        idCardString = idCardEditText.getText().toString().trim();
+
+        //Check Space
+        if (checkSpace()) {
+            //Have Space
+        } else {
+            //No Space
+        }
+
+    }   // clickSing
+
+    private boolean checkSpace() {
+
+        boolean bolResult = true;
+
+        bolResult = userString.equals("") ||
+                passwordString.equals("") ||
+                nameString.equals("") ||
+                idCardString.equals("");
+
+        return bolResult;
+    }
+
+}      // Main Class
