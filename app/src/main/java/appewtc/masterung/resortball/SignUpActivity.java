e7fec4369b3763866ba1021612bc19b901f6926e
masterUNG <phrombutr@gmail.com>
2016-03-31 10:55:03 +0700

    Can Update to mySQL finish

--- app/src/main/java/appewtc/masterung/resortball/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/resortball/SignUpActivity.java
@@ -1,9 +1,20 @@
 package appewtc.masterung.resortball;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -46,9 +57,41 @@ public class SignUpActivity extends AppCompatActivity {
 
         } else {
             //No Space
+            updateToMySQL();
         }
 
     }   // clickSing
+
+    private void updateToMySQL() {
+
+        //Connected Http
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            nameValuePairs.add(new BasicNameValuePair("User", userString));
+            nameValuePairs.add(new BasicNameValuePair("Password", passwordString));
+            nameValuePairs.add(new BasicNameValuePair("Name", nameString));
+            nameValuePairs.add(new BasicNameValuePair("IDcard", idCardString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/bang/php_add_user_master.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            httpClient.execute(httpPost);
+
+            Toast.makeText(this, "บันทึกขัอมูลเรียบร้อย ขอบคุณคะ", Toast.LENGTH_SHORT).show();
+            finish();
+
+        } catch (Exception e) {
+            Toast.makeText(this, "ไม่สามารถบันทึกข้อมูลได้", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }   // update
 
     private boolean checkSpace() {
 
