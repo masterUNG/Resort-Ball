c3558215f20e534d0ce3abbcf16a162a5104e230
masterUNG <phrombutr@gmail.com>
2016-03-31 13:50:46 +0700

    create OpenHelper finish

--- app/src/main/java/appewtc/masterung/resortball/MainActivity.java
+++ app/src/main/java/appewtc/masterung/resortball/MainActivity.java
@@ -4,14 +4,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
     }   // Main Method
+
+    public void clickSignInMain(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlertDialog myAlertDialog = new MyAlertDialog();
+            myAlertDialog.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
+        } else {
+
+        }
+
+    }   // clickSingIn
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+    }
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
---
+++ app/src/main/java/appewtc/masterung/resortball/MyOpenHelper.java
@@ -0,0 +1,48 @@
+package appewtc.masterung.resortball;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by masterUNG on 3/31/16 AD.
+ */
+public class MyOpenHelper extends SQLiteOpenHelper{
+
+    //Explicit
+    public static final String database_name = "myData.db";
+    private static final int database_version = 1;
+
+    private static final String create_user_table = "create table userTABLE (" +
+            "_id integer primary key, " +
+            "User text, " +
+            "Password text, " +
+            "Name text, " +
+            "IDcard text);";
+
+    private static final String create_service_table = "create table serviceTABLE (" +
+            "_id integer primary key, " +
+            "Category text, " +
+            "Image1 text, " +
+            "Image2 text, " +
+            "Image3 text, " +
+            "Image4 text, " +
+            "Description text, " +
+            "Price text, " +
+            "Item);";
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null, database_version);
+    }   // Constructor
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_user_table);
+        sqLiteDatabase.execSQL(create_service_table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}   // Main Class
