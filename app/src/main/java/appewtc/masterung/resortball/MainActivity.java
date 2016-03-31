ec8288fe77b8de224b1ee3cce641b0bd01898cea
masterUNG <phrombutr@gmail.com>
2016-03-31 16:02:47 +0700

    Authen Finish

--- app/src/main/java/appewtc/masterung/resortball/MainActivity.java
+++ app/src/main/java/appewtc/masterung/resortball/MainActivity.java
@@ -1,13 +1,15 @@
 package appewtc.masterung.resortball;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -165,10 +167,48 @@ public class MainActivity extends AppCompatActivity {
             MyAlertDialog myAlertDialog = new MyAlertDialog();
             myAlertDialog.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
         } else {
+            //No Space
+            checkUser();
 
         }
 
     }   // clickSingIn
+
+    private void checkUser() {
+
+        try {
+
+            SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                    MODE_PRIVATE, null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM userTABLE WHERE User = " + "'" + userString + "'", null);
+            cursor.moveToFirst();
+            String[] resultStrings = new String[cursor.getColumnCount()];
+            for (int i = 0; i < cursor.getColumnCount(); i++) {
+                resultStrings[i] = cursor.getString(i);
+            }
+            cursor.close();
+
+            //Check Password
+            if (passwordString.equals(resultStrings[2])) {
+                //Password True
+                Toast.makeText(this,
+                        "ยินดีต้อนรับ " + resultStrings[3],
+                        Toast.LENGTH_SHORT).show();
+
+            } else {
+                //Password False
+                MyAlertDialog myAlertDialog = new MyAlertDialog();
+                myAlertDialog.myDialog(this, "Password ผิด", "กรุณาพิมพ์ใหม่ Password ผิด");
+            }
+
+
+
+        } catch (Exception e) {
+            MyAlertDialog myAlertDialog = new MyAlertDialog();
+            myAlertDialog.myDialog(this, "User ผิด", "ไม่มี User นี่ในฐานข้อมูลของเรา");
+        }
+
+    }   // checkUser
 
     private void bindWidget() {
         userEditText = (EditText) findViewById(R.id.editText5);
