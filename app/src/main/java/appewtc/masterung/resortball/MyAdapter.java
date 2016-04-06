463a2e121ebab00506d4d06c2226b80385e8047e
masterUNG <phrombutr@gmail.com>
2016-04-06 09:54:38 +0700

    Create ListView Finish

---
+++ app/src/main/java/appewtc/masterung/resortball/MyAdapter.java
@@ -0,0 +1,68 @@
+package appewtc.masterung.resortball;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by masterUNG on 4/6/16 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context context;
+    private String[] iconStrings, priceStrings, nameStrings;
+
+    public MyAdapter(Context context,
+                     String[] iconStrings,
+                     String[] priceStrings,
+                     String[] nameStrings) {
+
+        this.context = context;
+        this.iconStrings = iconStrings;
+        this.priceStrings = priceStrings;
+        this.nameStrings = nameStrings;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return iconStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imageView2);
+        Picasso.with(context)
+                .load(iconStrings[i])
+                .resize(120, 120)
+                .into(iconImageView);
+
+        TextView priceTextView = (TextView) view1.findViewById(R.id.textView8);
+        priceTextView.setText(priceStrings[i]);
+
+        TextView nameTextView = (TextView) view1.findViewById(R.id.textView9);
+        nameTextView.setText(nameStrings[i]);
+
+        return view1;
+    }
+}   // Main Class
--- app/src/main/java/appewtc/masterung/resortball/ShowServiceActivity.java
+++ app/src/main/java/appewtc/masterung/resortball/ShowServiceActivity.java
@@ -1,5 +1,7 @@
 package appewtc.masterung.resortball;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,7 +27,39 @@ public class ShowServiceActivity extends AppCompatActivity {
         //Show View
         showView();
 
+        //Create ListView
+        createListView();
+
     } // Main Method
+
+    private void createListView() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + MyManage.service_table, null);
+        cursor.moveToFirst();
+        int intCount = cursor.getCount();
+
+        String[] iconStrings = new String[intCount];
+        String[] priceStrings = new String[intCount];
+        String[] nameStrings = new String[intCount];
+
+        for (int i = 0; i < intCount; i++) {
+
+            iconStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Image1));
+            priceStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Price));
+            nameStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Category));
+
+            cursor.moveToNext();
+        }   // for
+        cursor.close();
+
+        MyAdapter myAdapter = new MyAdapter(this,
+                iconStrings, priceStrings, nameStrings);
+        serviceListView.setAdapter(myAdapter);
+
+
+    }   // createListView
 
     private void showView() {
         nameString = getIntent().getStringExtra("NameUser");
